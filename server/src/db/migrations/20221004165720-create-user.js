b6ba33b36efc193df809e6e377f646c5ea73eb9a
Roman Rudenko <ruroma18@gmail.com>
2022-10-04 20:25:39 +0300

    Add User model and migration

---
+++ server/src/db/migrations/20221004165720-create-user.js
@@ -0,0 +1,52 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        filed: 'first_name',
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      lastName: {
+        filed: 'last_name',
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING
+      },
+      role: {
+        type: Sequelize.ENUM('admin', 'teacher', 'student')
+      },
+      photo: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        field: 'created_at',
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        field: 'updated_at',
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
---
+++ server/src/db/models/user.js
@@ -0,0 +1,63 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    firstName: {
+      field: 'first_name',
+      allowNull: false,
+      type: DataTypes.STRING,
+      validate: {
+        isAlpha: true,
+        notEmpty: true,
+        notNull: true
+      }
+    },
+    lastName: {
+      field: 'last_name',
+      allowNull: false,
+      type: DataTypes.STRING,
+      validate: {
+        isAlpha: true,
+        notEmpty: true,
+        notNull: true
+      }
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      validate: {
+        isAlphanumeric: true,
+        notEmpty: true,
+        notNull: true
+      }
+    },
+    email: {
+      allowNull: false,
+      unique: true,
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true,
+        notEmpty: true,
+        notNull: true
+      }
+    },
+    role: {
+      allowNull: false,
+      type: DataTypes.ENUM('admin', 'teacher', 'student')
+    },
+    photo: {
+      type: DataTypes.STRING
+    },
+  }, {
+    underscored: true,
+    sequelize,
+    modelName: 'User',
+    tableName: 'users'
+  });
+  return User;
+};
