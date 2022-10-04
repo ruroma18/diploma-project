003c6330049e03e1b1b2a68155cf2be3763f98b8
Roman Rudenko <ruroma18@gmail.com>
2022-10-04 16:28:53 +0300

    Initial commit

---
+++ server/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const router = require('./routers');
+
+const app = express();
+
+PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use('/public', express.static('public'));
+app.use(router);
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => console.log(`Server started at ${PORT} port`));
