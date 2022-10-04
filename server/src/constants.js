6c9b2d61529b1cebc0e3bd803c8f5da56abbf8bd
Roman Rudenko <ruroma18@gmail.com>
2022-10-04 20:25:16 +0300

    Fix constants

--- server/src/constants.js
+++ server/src/constants.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   configPath: path.resolve(__dirname, 'config', 'postgresConfig.json'),
-  modelsPath: path.resolve(__dirname, 'models'),
-  seedersPath: path.resolve(__dirname, 'seeders'),
-  migrationsPath: path.resolve(__dirname, 'migrations')
+  modelsPath: path.resolve(__dirname, 'db', 'models'),
+  seedersPath: path.resolve(__dirname, 'db', 'seeders'),
+  migrationsPath: path.resolve(__dirname, 'db', 'migrations')
 }
