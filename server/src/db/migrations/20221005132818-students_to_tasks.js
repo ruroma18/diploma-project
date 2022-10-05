9505bb50162f1c758a551898b6720f2199944c4b
Roman Rudenko <ruroma18@gmail.com>
2022-10-05 16:59:20 +0300

    Add students_to_tasks migation

---
+++ server/src/db/migrations/20221005132818-students_to_tasks.js
@@ -0,0 +1,57 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('students_to_tasks', {
+      studentId: {
+        field: 'student_id',
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'users',
+            key: 'id'
+          }
+        },
+        onDelete: 'cascade',
+        onUpdate: 'cascade'
+      },
+      taskId: {
+        field: 'task_id',
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'tasks',
+            key: 'id'
+          }
+        },
+        onDelete: 'cascade',
+        onUpdate: 'cascade'
+      },
+      isDone: {
+        field: 'is_done',
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      createdAt: {
+        field: 'created_at',
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        field: 'updated_at',
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('students_to_tasks');
+  }
+};
