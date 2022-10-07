bddafaaa8e63aa57ec75e37c8c561b7be481cb98
Roman Rudenko <ruroma18@gmail.com>
2022-10-07 13:31:45 +0300

    Add contants

--- server/src/constants.js
+++ server/src/constants.js
@@ -1,8 +1,25 @@
 const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '..', '.env') })
+
+const {
+  JWT_ACCESS_SECRET,
+  JWT_ACCESS_TIME,
+  JWT_REFRESH_SECRET,
+  JWT_REFRESH_TIME
+
+} = process.env;
 
 module.exports = {
   configPath: path.resolve(__dirname, 'config', 'postgresConfig.json'),
   modelsPath: path.resolve(__dirname, 'db', 'models'),
   seedersPath: path.resolve(__dirname, 'db', 'seeders'),
-  migrationsPath: path.resolve(__dirname, 'db', 'migrations')
+  migrationsPath: path.resolve(__dirname, 'db', 'migrations'),
+  JWT_ACCESS_SECRET,
+  JWT_ACCESS_TIME,
+  JWT_REFRESH_SECRET,
+  JWT_REFRESH_TIME,
+  TEACHER: 'teacher',
+  STUDENT: 'student',
+  VIDEO: 'video',
+  FILE: 'file'
 }
