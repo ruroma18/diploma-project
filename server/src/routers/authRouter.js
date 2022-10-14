d8e6e9d31662e4edae272d500f4dae7aed29166c
Roman Rudenko <ruroma18@gmail.com>
2022-10-14 11:29:44 +0300

    Add checkRefreshToken to router path

--- server/src/routers/authRouter.js
+++ server/src/routers/authRouter.js
@@ -1,8 +1,9 @@
 const authRouter = require('express').Router();
 const AuthController = require('../controllers/authController');
+const { checkRefreshToken } = require('../middleware/tokenMW');
 
 authRouter.post('/register', AuthController.register);
 authRouter.post('/login', AuthController.login);
-authRouter.post('/refresh', AuthController.refresh)
+authRouter.post('/refresh', checkRefreshToken, AuthController.refresh)
 
 module.exports = authRouter;
