62e67b019f74593e02a157c2a0e571c439acbc21
Roman Rudenko <ruroma18@gmail.com>
2022-10-14 11:28:56 +0300

    Add chekcRefreshToken

--- server/src/middleware/tokenMW.js
+++ server/src/middleware/tokenMW.js
@@ -1,17 +1,37 @@
 const jwtService = require('../services/jwtService');
 
-module.exports.checkToken = async (req, res, send) => {
+module.exports.checkAccessToken = async (req, res, next) => {
   try {
-    const {headers: {autorization}} = req;
+    const { headers: { autorization } } = req;
 
     const [authType, token] = autorization.split(' ');
 
-    const verifiedToken = await jwtService.verifyAccessToken(token);
+    await jwtService.verifyAccessToken(token);
 
     next();
   } catch (error) {
-    next(error)
-    
+    next(error);
   }
+}
 
+module.exports.checkRefreshToken = async (req, res, next) => {
+  try {
+
+    const { body: { refreshToken } } = req;
+
+    const refreshTokenInstance = await RefreshToken.findOne({ where: { token: refreshToken } });
+
+    if(!refreshTokenInstance) {
+      return next(createHttpError(401, 'Invalid data'));
+    }
+
+    await jwtService.checkRefreshToken(refreshToken);
+
+    req.refreshTokenInstance = refreshTokenInstance;
+
+    next();
+  } catch (error) {
+    next(error);
+
+  }
 }
