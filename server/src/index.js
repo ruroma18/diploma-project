62298e556f78a4f898ca81d13e7cadcbce828b43
Roman Rudenko <ruroma18@gmail.com>
2022-10-14 11:30:02 +0300

    Add errorHandlers to api

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const router = require('./routers');
+const { tokenErrorHandler, basicErrorHandler } = require('./middleware/errorHandler');
 
 const app = express();
 
@@ -11,6 +12,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/public', express.static('public'));
 app.use('/api', router);
+app.use(tokenErrorHandler);
+app.use(basicErrorHandler);
 
 const server = http.createServer(app);
 
