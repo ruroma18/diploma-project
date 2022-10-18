018dfddbd9812c9e7ebdc1598767e89066fa6092
Roman Rudenko <ruroma18@gmail.com>
2022-10-18 18:11:18 +0300

    Refactoring authThunk

--- client/src/redux/features/auth/authThunk.js
+++ client/src/redux/features/auth/authThunk.js
@@ -1,29 +1,31 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { getToken, removeToken, setToken } from "utils/helperFunctions";
+import { getToken, removeTokens, setToken } from "utils/helperFunctions";
 import history from "utils/history";
 import api from '../../../api/http';
+import CONSTANTS from '../../../constants';
 
 export const fetchUserData = createAsyncThunk('auth/fetchUserData',
   async (data, { rejectWithValue }) => {
     try {
-      const accessToken = getToken();
-      api.defaults.headers.Authorization = `Bearer ${accessToken}`;
-      const response = await api.get('/auth/user');
-      return { ...response.data, accessToken };
+      const accessToken = getToken(CONSTANTS.ACCESS_TOKEN);
+      api.defaults.headers.authorization = `Bearer ${accessToken}`;
+      const response = await api.get('auth/getUser');
+      return response.data;
     } catch (error) {
-      removeToken();
-      return rejectWithValue('');
+      removeTokens();
+      rejectWithValue(error);
     }
   });
 
 export const login = createAsyncThunk('auth/login',
   async(payload) => {
-    const response = await api.post('/auth/login', payload);
-    setToken(response.data.tokenPair.accessToken);
+    const response = await api.post('auth/login', payload);
+    setToken(CONSTANTS.ACCESS_TOKEN, response.data.tokenPair.accessToken);
+    setToken(CONSTANTS.REFRESH_TOKEN, response.data.tokenPair.refreshToken);
     history.push(`/${response.data.role}`);
     return response.data;
   });
 
 export const signOut = createAsyncThunk('auth/signOut', async() => {
-  removeToken();
+  removeTokens();
 }); 
