cc5b966cfe09ceabbe630f9586042fdb0d4e01bc
Roman Rudenko <ruroma18@gmail.com>
2022-10-19 17:29:59 +0300

    Refactoring privateRoute

--- client/src/utils/privateRoute.js
+++ client/src/utils/privateRoute.js
@@ -2,17 +2,20 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import Loading from 'components/Loading/Loading';
 import { useNavigate } from 'react-router-dom';
+import { getToken } from './helperFunctions';
+import CONSTANTS from '../constants'
 
 const PrivateRoute = ({ children }) => {
   const navigate = useNavigate();
   const { loading, userData } = useSelector((state) => state.auth);
+  const token = getToken(CONSTANTS.ACCESS_TOKE)
+
+  if (!userData && !token) {
+    return navigate('/login');
+  }
 
   if (loading) {
     return <Loading />
-  }
-
-  if (!userData) {
-    return navigate('/login');
   }
 
   return children;
