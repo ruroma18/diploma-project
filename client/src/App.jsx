9ecc51f0b0146ea819c0467221eb036d66a2e362
Roman Rudenko <ruroma18@gmail.com>
2022-10-19 17:29:45 +0300

    Delete history

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,48 +5,62 @@ import TeacherPage from "pages/Teacher";
 import CoursePage from "pages/Course";
 import Task from "pages/Task";
 import PrivateRoute from "utils/privateRoute";
-import history from "utils/history";
-import { getToken } from 'utils/helperFunctions';
-import { fetchUserData } from 'redux/features/auth/authThunk';
+import { getToken } from "utils/helperFunctions";
+import { fetchUserData } from "redux/features/auth/authThunk";
 import { useDispatch } from "react-redux";
 import CONSTANTS from "./constants";
+import Registration from "components/Registration";
+import LoginPage from "pages/Login";
 
 function App() {
-
   const dispatch = useDispatch();
 
-  if(getToken(CONSTANTS.ACCESS_TOKEN)) {
+  if (getToken(CONSTANTS.ACCESS_TOKEN)) {
     dispatch(fetchUserData());
   }
 
   return (
-    <Router history={history}>
+    <Router>
       <Routes>
-        <Route exact path="/login" element={<MainPage />} />
-        <Route exact path="/teacher" element={
-          <PrivateRoute>
-            <TeacherPage />
-          </PrivateRoute>
-        }>
-        </Route>
-        <Route exact path="/student" element={
-          <PrivateRoute>
-            <StudentPage />
-          </PrivateRoute>
-        }>
-        </Route>
-        <Route exact path="/course" element={
-          <PrivateRoute>
-            <CoursePage />
-          </PrivateRoute>
-        }>
-        </Route>
-        <Route exact path="/task" element={
-          <PrivateRoute>
-            <Task />
-          </PrivateRoute>
-        }>
-        </Route>
+        <Route exact path="/" element={<MainPage />} />
+        <Route exact path="/login" element={<LoginPage />} />
+        <Route exact path="/register" element={<Registration />} />
+        <Route
+          exact
+          path="/teacher"
+          element={
+            <PrivateRoute>
+              <TeacherPage />
+            </PrivateRoute>
+          }
+        ></Route>
+        <Route
+          exact
+          path="/student"
+          element={
+            <PrivateRoute>
+              <StudentPage />
+            </PrivateRoute>
+          }
+        ></Route>
+        <Route
+          exact
+          path="/course"
+          element={
+            <PrivateRoute>
+              <CoursePage />
+            </PrivateRoute>
+          }
+        ></Route>
+        <Route
+          exact
+          path="/task"
+          element={
+            <PrivateRoute>
+              <Task />
+            </PrivateRoute>
+          }
+        ></Route>
       </Routes>
     </Router>
   );
