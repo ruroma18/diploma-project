497e4b1ea5954959beb708165fcb24d25211c2b1
Roman Rudenko <ruroma18@gmail.com>
2022-10-20 18:59:18 +0300

    Add courseController

---
+++ server/src/controllers/courseController.js
@@ -0,0 +1,35 @@
+const { Course } = require('../db/models');
+const CONSTANTS = require('../constants');
+
+module.exports.getCourses = async (req, res, next) => {
+  try {
+    const { user } = req;
+
+    if(user.role === CONSTANTS.TEACHER) {
+      const courses = await user.findCourse();
+      res.status(200).send(courses);
+    }
+    
+    if(user.role === CONSTANTS.STUDENT) {
+      const courses = await Course.findAll();
+      res.status(200).send(courses);
+    }
+    
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports.createCourse = async (req, res, next) => {
+  try {
+
+    const { body, user} = req;
+
+    const course = await user.createCourse(body);
+
+    res.status(200).send(course)
+    
+  } catch (error) {
+    next(error);
+  }
+}
