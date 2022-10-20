f3b948843126b714f807a28b1dd95c197f316f13
Roman Rudenko <ruroma18@gmail.com>
2022-10-20 16:11:29 +0300

    Add modal dialog for creation new course

--- client/src/components/AddNewCourse /index.jsx
+++ client/src/components/AddNewCourse /index.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
-import styles from './AddNewCourse.module.scss'
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import React, { useEffect, useState } from "react";
+import styles from "./AddNewCourse.module.scss";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import Dialog from "@mui/material/Dialog";
+import CourseForm from "components/CourseForm";
+import CloseIcon from '@mui/icons-material/Close';
 
 const AddNewCourse = () => {
+  const [openModal, setOpenModal] = useState(false);
+
+  useEffect(() => {}, [openModal]);
+
+  const toggleOpenModal = () => setOpenModal(openModal === true ? false : true);
+
   return (
-    <div className={styles.container}>
-      <AddCircleOutlineIcon className={styles.addIcon}/>      
+    <div className={styles.container} >
+      <AddCircleOutlineIcon className={styles.addIcon} onClick={() => toggleOpenModal()}/>
+      <Dialog open={openModal} className={styles.modalContainer} >
+        <CloseIcon className={styles.modalBtn} onClick={() => toggleOpenModal()}/>
+        <h2 className={styles.modalHeading}>Створити курс</h2>
+          <CourseForm />
+      </Dialog>
     </div>
   );
-}
+};
 
 export default AddNewCourse;
