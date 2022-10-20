5f6f946daef5d37cdb72f2cbaa70cb8082985498
Roman Rudenko <ruroma18@gmail.com>
2022-10-20 16:12:18 +0300

    Add inputImage component

---
+++ client/src/components/InputComponents/InputImage/InputImage.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import styles from "./InputImage.module.scss";
+import { Field } from "formik";
+
+const InputImage = (name) => {
+  const [file, setFile] = useState();
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setFile(URL.createObjectURL(e.target.files[0]));
+  };
+
+  return (
+    <div className={styles.imageUploadContainer}>
+      <Field
+        name={name}
+        type="file"
+        className={styles.field}
+        onChange={handleChange}
+      ></Field>
+
+      {file ? (
+        <div className={styles.imgWrapper}>
+          <img
+            className={styles.imagePreview}
+            id="imagePreview"
+            src={file}
+            alt="uploadedImage"
+          />
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default InputImage;
