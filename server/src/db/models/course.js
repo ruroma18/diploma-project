8855e8e2d2b7291b6c9b2018e1a3d75b5f5d0df5
Roman Rudenko <ruroma18@gmail.com>
2022-10-21 18:19:03 +0300

    Fix course model

--- server/src/db/models/course.js
+++ server/src/db/models/course.js
@@ -17,7 +17,6 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        isAlphanumeric: true,
         notEmpty: true,
         notNull: true
       }
