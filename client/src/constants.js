b6f0ff35c9a902f2489083bbccd5bee59296bfc6
Roman Rudenko <ruroma18@gmail.com>
2022-10-21 18:16:27 +0300

    Add contants for img

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,7 +1,8 @@
 const CONSTANTS = {
   BASE_URL: 'http://localhost:5000/api/',
   ACCESS_TOKEN: 'accessToken',
-  REFRESH_TOKEN: 'refreshToken'
+  REFRESH_TOKEN: 'refreshToken',
+  PUBLIC_URL: 'http://localhost:5000/public/images/'
 }
 
 export default CONSTANTS;
