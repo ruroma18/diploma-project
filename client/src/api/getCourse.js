95443c0bf9ee3578c51678bb3b1afdd0325a265b
Roman Rudenko <ruroma18@gmail.com>
2022-10-23 15:31:40 +0300

    Delete unuseless files

--- client/src/api/getCourse.js
+++
@@ -1,5 +0,0 @@
-const getCourse = () => fetch("/cibersecurity.json")
-.then((res) => res.json())
-.catch((err => console.log(err)))
-
-export default getCourse;
--- client/src/api/index.js
+++
@@ -1,2 +0,0 @@
-export { default as getUsers } from './getUser';
-export { default as getCourse} from './getCourse';
