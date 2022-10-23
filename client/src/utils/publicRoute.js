67bf45eed0fb08fdea603207e9db69592ce8e571
Roman Rudenko <ruroma18@gmail.com>
2022-10-23 15:29:54 +0300

    Add public route

---
+++ client/src/utils/publicRoute.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Loading from 'components/Loading/Loading';
+import { Navigate } from 'react-router-dom';
+import { getToken } from './helperFunctions';
+import CONSTANTS from '../constants'
+
+const PublicRoute = ({ children }) => {
+  const { loading, userData } = useSelector((state) => state.auth);
+  const token = getToken(CONSTANTS.ACCESS_TOKEN)
+
+  if (userData && token) {
+    return <Navigate to={`/${userData.role}`}/>
+  }
+
+  if (loading) {
+    return <Loading />
+  }
+
+  return children;
+}
+
+export default PublicRoute;
