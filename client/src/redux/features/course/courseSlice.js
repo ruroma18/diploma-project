ff3f5f43f5315705a92e41c1fede269a87bce708
Roman Rudenko <ruroma18@gmail.com>
2022-10-24 11:33:28 +0300

    Add extraReducers to courseSlice

--- client/src/redux/features/course/courseSlice.js
+++ client/src/redux/features/course/courseSlice.js
@@ -1,16 +1,41 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { createCourse, getCourses } from "./courseThunk";
 
 const initialState = {
   courseData: null,
   loading: null,
-  error:null,
+  error: null,
 }
 
 export const courseSlice = createSlice({
   name: 'course',
   initialState,
   reducers: {},
-  extraReducers: {}
+  extraReducers: {
+    [createCourse.pending]: (state, action) => {
+      state.loading = true;
+      state.courseData = null;
+    },
+    [createCourse.fulfilled]: (state, action) => {
+      state.loading = false;
+    },
+    [createCourse.rejected]: (state, action) => {
+      state.loading = false;
+      state.courseData = null;
+    },
+    [getCourses.pending]: (state, action) => {
+      state.loading = true;
+      state.courseData = null;
+    },
+    [getCourses.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.courseData =  action.payload;
+    },
+    [getCourses.rejected]: (state, action) => {
+      state.loading = false;
+      state.courseData = null;
+    },
+  }
 });
 
 export default courseSlice.reducer;
