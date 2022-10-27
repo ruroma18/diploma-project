73e841ac56f8845830a91abf388a8db8cf104f69
Roman Rudenko <ruroma18@gmail.com>
2022-10-27 11:57:02 +0300

    Fix validation in section model

--- server/src/db/models/section.js
+++ server/src/db/models/section.js
@@ -13,7 +13,6 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        isAlphanumeric: true,
         notEmpty: true,
         notNull: true
       }
