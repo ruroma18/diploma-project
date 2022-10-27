936c7c90ba5c4c27cb7f87b683fd2845dff2d126
Roman Rudenko <ruroma18@gmail.com>
2022-10-27 23:09:49 +0300

    Refactoring material controller and router

--- server/src/controllers/materialController.js
+++ server/src/controllers/materialController.js
@@ -1,10 +1,17 @@
+const { Section, Material } = require('../db/models');
+
 module.exports.getMaterials = async (req, res, next) => {
   try {
-    const { section } = req;
+    const { course } = req;
 
-    const material = await section.getMaterials();
+    const sectionsId = await Section.findAll({ 
+      where: { courseId: course.id }, 
+      attributes: ['id'] })
+      .then(sectionsId => sectionsId.map(id => id.id));
 
-    res.status(200).send(material);
+    const materials = await Material.findAll({where: {sectionId: sectionsId}});
+
+    res.status(200).send(materials);
   } catch (error) {
     next(error);
   }
@@ -12,9 +19,12 @@ module.exports.getMaterials = async (req, res, next) => {
 
 module.exports.createMaterials = async (req, res, next) => {
   try {
+
     const { section, body, file: { filename } } = req;
 
     const material = await section.createMaterial({ filePath: filename, ...body });
+
+    console.log(material)
 
     res.status(200).send(material)
 
--- server/src/routers/materialRouter.js
+++ server/src/routers/materialRouter.js
@@ -1,10 +1,11 @@
 const materialRouter = require('express').Router();
 const { checkAccessToken } = require('../middleware/tokenMW');
 const { sectionMW } = require('../middleware/sectionMW');
+const { courseMW } = require('../middleware/courseMW');
 const MaterialController = require('../controllers/materialController');
 const fileUpload = require('../utils/fileUpload');
 
-materialRouter.get('/getMaterials', checkAccessToken, sectionMW, MaterialController.getMaterials);
+materialRouter.get('/getMaterials', checkAccessToken, courseMW, MaterialController.getMaterials);
 materialRouter.post('/createMaterial',
   checkAccessToken,
   sectionMW,
