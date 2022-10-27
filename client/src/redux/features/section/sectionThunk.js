a1ea49c9cc7105e6f857c1add09bc72b62dd7591
Roman Rudenko <ruroma18@gmail.com>
2022-10-27 16:47:14 +0300

    Delete console logs

--- client/src/redux/features/section/sectionThunk.js
+++ client/src/redux/features/section/sectionThunk.js
@@ -4,13 +4,11 @@ import api from '../../../api/http';
 export const getSections = createAsyncThunk('section/getSections',
   async (payload) => {
     const response = await api.get('section/getSections', {params: {id: payload}})
-    console.log(response)
     return response.data;
   });
 
 export const createSection = createAsyncThunk('section/createSection',
   async (payload) => {
-    console.log(payload)
     await api.post('section/createSection', payload, {params: {id: payload.courseId}} );
     window.location.reload();
   });  
