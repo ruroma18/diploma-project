e2d542310b19bf843b43ebe33b488a38803bda4b
Roman Rudenko <ruroma18@gmail.com>
2022-10-27 11:58:27 +0300

    Refactoring course content

--- client/src/components/CourseContent/index.jsx
+++ client/src/components/CourseContent/index.jsx
@@ -1,43 +1,31 @@
 import React, { useEffect } from "react";
-import ArticleIcon from "@mui/icons-material/Article";
-import TaskIcon from "@mui/icons-material/Task";
-import styles from "./CourseContent.module.scss";
 import { useDispatch, useSelector } from "react-redux";
-import { getSections } from "redux/features/section/sectionThunk";
+import AddNewSection from "components/AddNewSection";
+import { useParams } from "react-router-dom";
+import { getCourseById } from "redux/features/course/courseThunk";
+import CourseSection from "components/CourseSection";
+import styles from "./CourseContent.module.scss";
 
 const CourseContent = () => {
   const dispatch = useDispatch();
+  const { courseId } = useParams();
 
-  const { selectedCourse } = useSelector((state) => state.course);
+  const {
+    course: { selectedCourse },
+    auth: { userData },
+  } = useSelector((state) => state);
 
   useEffect(() => {
-    dispatch(getSections(selectedCourse.id));
-  }, [dispatch, selectedCourse.id]);
-
-  const { sectionData } = useSelector((state) => state.section);
+    dispatch(getCourseById(courseId));
+  }, [dispatch, courseId]);
 
   return (
     <article className={styles.container}>
       <h2 className={styles.mainHeading}>{selectedCourse.name}</h2>
-      <ul>
-        {sectionData.map((section) => (
-          <li key={`${section.id}`}>
-            <h3 className={styles.itemHeading}>
-              Розділ {section.id}. {section.name}
-            </h3>
-            <div className={styles.materialWrapper}>
-              <ArticleIcon />
-              <a className={styles.materialLink} href="#">
-                Лекція
-              </a>
-            </div>
-            <div className={styles.materialWrapper}>
-              <TaskIcon />
-              <p className={styles.materialLink}>Завдання до розділу</p>
-            </div>
-          </li>
-        ))}
-      </ul>
+      {userData.role === "teacher" ? (
+        <AddNewSection courseId={courseId} />
+      ) : null}
+      <CourseSection courseId={courseId}/>
     </article>
   );
 };
---
+++ client/src/components/CourseSection/index.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from "react";
+import ArticleIcon from "@mui/icons-material/Article";
+import TaskIcon from "@mui/icons-material/Task";
+import { useDispatch, useSelector } from "react-redux";
+import { getSections } from "redux/features/section/sectionThunk";
+import styles from "./courseSection.module.scss";
+
+const CourseSection = ({ courseId }) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getSections(courseId));
+  }, [dispatch, courseId]);
+
+  const { sectionData } = useSelector((state) => state.section);
+
+  return (
+    <ul>
+      {sectionData.map((section) => (
+        <li key={`${section.id}`}>
+          <h3 className={styles.itemHeading}>
+            Розділ {section.id}. {section.name}
+          </h3>
+          <div className={styles.materialWrapper}>
+            <ArticleIcon />
+            <a className={styles.materialLink} href="#">
+              Лекція
+            </a>
+          </div>
+          <div className={styles.materialWrapper}>
+            <TaskIcon />
+            <p className={styles.materialLink}>Завдання до розділу</p>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default CourseSection;
