606c6352c2e3247608b41c9f9d8a11d3565cad8f
Roman Rudenko <ruroma18@gmail.com>
2022-11-08 13:29:19 +0200

    Add delete icon to course cards

--- client/src/components/CourseCard/index.jsx
+++ client/src/components/CourseCard/index.jsx
@@ -1,29 +1,46 @@
 import React from "react";
 import CONSTANTS from "../../constants";
-import styles from "./CourseCard.module.scss";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import DeleteIcon from "@mui/icons-material/Delete";
+import styles from "./CourseCard.module.scss";
 
-const CourseCard = () => {
+const CourseCard = ({ userRole }) => {
   const navigate = useNavigate();
-  const {courseData} = useSelector(state => state.course);
-  
+  const { courseData } = useSelector((state) => state.course);
 
   const getCourse = (id) => {
-    navigate(`/course/${id}`);    
+    navigate(`/course/${id}`);
+  };
+
+  const deleteCourse = (id) => {
+    console.log(id)
   }
 
   return (
     <>
       {courseData.map((course) => (
-        <div className={styles.cardWrapper} key={course.id} onClick={() => getCourse(course.id)}>
+        <div className={styles.cardWrapper}>
+          <div
+            className={styles.imgWrapper}
+            key={course.id}
+            onClick={() => getCourse(course.id)}
+          >
             <img
               src={`${CONSTANTS.PUBLIC_IMAGE_URL}${course.imgPath}`}
               alt={course.name}
               className={styles.cardImg}
             />
-          <div className={styles.cardImg}></div>
-          <h2 className={styles.heading}>{course.name}</h2>
+          </div>
+          <div className={styles.nameContainer}>
+            <h2 className={styles.heading}>{course.name}</h2>
+            {userRole === CONSTANTS.TEACHER ? (
+              <DeleteIcon
+                className={styles.deleteIcon}
+                onClick={() => deleteCourse(course.id)}
+              />
+            ) : null}
+          </div>
         </div>
       ))}
     </>
--- client/src/components/CoursesDashboard/index.jsx
+++ client/src/components/CoursesDashboard/index.jsx
@@ -3,6 +3,7 @@ import CourseCard from "components/CourseCard";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getCourses } from "redux/features/course/courseThunk";
+import CONSTANTS from '../../constants';
 import styles from "./CoursesDashboard.module.scss";
 
 const CoursesDashboard = () => {
@@ -17,8 +18,8 @@ const CoursesDashboard = () => {
     <article className={styles.container}>
       <h2 className={styles.heading}>Курси</h2>
       <div className={styles.courseContainer}>
-        <CourseCard />
-        {userData.role === "teacher" ? <AddNewCourse /> : null}
+        <CourseCard userRole={userData.role} />
+        {userData.role === CONSTANTS.TEACHER ? <AddNewCourse /> : null}
       </div>
     </article>
   );
